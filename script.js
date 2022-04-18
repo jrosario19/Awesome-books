d3decca66c97d494a6130deeac551a54e7d75561
Juan Rosario <jrosario19@hotmail.com>
2022-04-18 14:22:50 -0400

    Add add and remove book from list

---
+++ script.js
@@ -0,0 +1,48 @@
+function retrieveBooks() {
+  const books = JSON.parse(localStorage.getItem('Books'));
+  document.getElementById('books-list').innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    document.getElementById('books-list').innerHTML
+          += `<li>
+              <p>${books[i].title}</p>
+              <p>${books[i].author}</p>
+              <button type="button" onclick="removeBook(${i})">Remove</button>
+          </li>`;
+  }
+}
+
+function createBook(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+
+  const book = {
+    title,
+    author,
+  };
+
+  if (localStorage.getItem('Books') === null) {
+    const books = [];
+    books.push(book);
+    localStorage.setItem('Books', JSON.stringify(books));
+  } else {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    books.push(book);
+    localStorage.setItem('Books', JSON.stringify(books));
+  }
+  retrieveBooks();
+  document.getElementById('form').reset();
+}
+
+document.getElementById('form').addEventListener('submit', createBook);
+
+retrieveBooks();
+
+function removeBook(i) {
+  const books = JSON.parse(localStorage.getItem('Books'));
+  books.splice(i, 1);
+  localStorage.setItem('Books', JSON.stringify(books));
+  retrieveBooks();
+}
+
+removeBook(1000000);
