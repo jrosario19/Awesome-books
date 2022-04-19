77d96b523847a42fc4faf15747872905403d9237
Juan Rosario <jrosario19@hotmail.com>
2022-04-19 16:17:33 -0400

    Update index

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
-function retrieveBooks() {
-  const books = JSON.parse(localStorage.getItem('Books'));
-  document.getElementById('books-list').innerHTML = '';
-  for (let i = 0; i < books.length; i += 1) {
-    document.getElementById('books-list').innerHTML
-          += `<p>${books[i].title}</p>
-              <p>${books[i].author}</p>
-              <button type="button" onclick="removeBook(${i})">Remove</button>
-              <hr>
-              `;
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-}
 
-function createBook(e) {
-  e.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-
-  const book = {
-    title,
-    author,
-  };
-
-  if (localStorage.getItem('Books') === null) {
-    const books = [];
-    books.push(book);
-    localStorage.setItem('Books', JSON.stringify(books));
-  } else {
+  static retrieveBooks() {
     const books = JSON.parse(localStorage.getItem('Books'));
-    books.push(book);
-    localStorage.setItem('Books', JSON.stringify(books));
+    document.getElementById('tbody').innerHTML = '';
+    for (let i = 0; i < books.length; i += 1) {
+      document.getElementById('tbody').innerHTML
+            += `<tr ><td class="book-container">"${books[i].title}" by ${books[i].author} <button class="btn" type="button" onclick="Book.removeBook(${i})">Remove</button></td></tr>`;
+    }
   }
-  retrieveBooks();
-  document.getElementById('form').reset();
+
+  static createBook(e) {
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+
+    const book = new Book(title, author);
+
+    if (localStorage.getItem('Books') === null) {
+      const books = [];
+      books.push(book);
+      localStorage.setItem('Books', JSON.stringify(books));
+    } else {
+      const books = JSON.parse(localStorage.getItem('Books'));
+      books.push(book);
+      localStorage.setItem('Books', JSON.stringify(books));
+    }
+    Book.retrieveBooks();
+    document.getElementById('form').reset();
+  }
+
+  static removeBook(i) {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    books.splice(i, 1);
+    localStorage.setItem('Books', JSON.stringify(books));
+    Book.retrieveBooks();
+  }
 }
-
-document.getElementById('form').addEventListener('submit', createBook);
-
-retrieveBooks();
-function removeBook(i) {
-  const books = JSON.parse(localStorage.getItem('Books'));
-  books.splice(i, 1);
-  localStorage.setItem('Books', JSON.stringify(books));
-  retrieveBooks();
-}
-
-removeBook(1000000);
+document.getElementById('form').addEventListener('submit', Book.createBook);
+Book.retrieveBooks();
